f1eac1d8f37085e0ff4f34476f1435801727fe9f
Pengdwende Abdoul GUira <guimani@yahoo.fr>
2022-01-15 20:30:46 -0500

    all the functionalities are ok for Assignment_06_Project_02

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react'
-import Header from './Header';
-import Inputbox from './Inputbox';
-import Button from './Button';
-import DisplayLabel from './DisplayLabel';
+import Header from './components/Header';
+import CityInput from './components/CityInput';
+
 
 
 export default function App() {
@@ -10,9 +9,9 @@ export default function App() {
     <div>
 
       <Header text="Zip Code Search" color="red" />
-      <Inputbox />
-      <Button text="Find Cities" color="green" />
-      <DisplayLabel />
+      <CityInput></CityInput>
+
+      
 
      
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   
@@ -10,4 +10,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-reportWebVitals();
+
