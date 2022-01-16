875775535af0aea0bef164cd7fe207db0b23becf
Pengdwende Abdoul GUira <guimani@yahoo.fr>
2022-01-15 20:08:28 -0500

    Created Component ZipCode.js that has a props representing a zipcode value

---
+++ src/components/ZipCode.js
@@ -0,0 +1,16 @@
+import React from "react"
+export default function ZipCode(props) {
+    return (
+        <div>
+            
+            <ul>
+
+               <li>{`ZipCode: ${props.entry}`}</li>
+
+            </ul>
+            
+                
+            
+        </div>
+    )
+}
